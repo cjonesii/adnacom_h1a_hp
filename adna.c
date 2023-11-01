bd5a9ac0c6a912cfc649a3ca51e6996c228a1ca1
Carlos Jones <cjonesii@yahoo.com>
2023-11-02 07:54:38 +0800

    1. PCI Bus Status

--- adna.c
+++ adna.c
@@ -982,7 +982,7 @@ show_verbose(struct device *d)
   if ((FLAG(cmd, PCI_COMMAND_IO) == '-') ||
       (FLAG(cmd, PCI_COMMAND_MEMORY) == '-') ||
       (FLAG(cmd, PCI_COMMAND_MASTER) == '-') ) {
-    set_conf_byte(d, PCI_COMMAND, 0x3);
+    set_conf_byte(d, PCI_COMMAND, 0x7);
   }
 
   pci_fill_info(p, PCI_FILL_IRQ | PCI_FILL_BASES | PCI_FILL_ROM_BASE | PCI_FILL_SIZES |
